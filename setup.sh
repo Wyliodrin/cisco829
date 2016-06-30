93254a2e5980431967c43e3efc22f36181dd61ee
Razvan <raz@raz.raz>
2016-06-30 12:49:09 +0300

    added redis

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 mkdir /opt
 
+#######start node
 cd node
 
 cp -a bin/. /usr/bin/
@@ -10,20 +11,49 @@ cp -a include/. /usr/include/
 cp -a share/. /usr/share/
 
 cd ..
+#####end node
 
+#####start redis
+cd redis
+
+cp redis-cli /usr/bin/redis-cli
+cp redis-server /usr/bin/redis-server
+
+cp redis /etc/init.d/redis
+chmod +x /etc/init.d/redis
+
+ln -s ../init.d/redis /etc/rc2.d/S98redis
+ln -s ../init.d/redis /etc/rc3.d/S98redis
+ln -s ../init.d/redis /etc/rc4.d/S98redis
+ln -s ../init.d/redis /etc/rc5.d/S98redis
+
+cd ..
+#end redis
+
+
+#####start wyliodrin-app-server
 mkdir /opt/wyliodrin
-cp -r wyl /opt/wyliodrin/wyl
+cp -rp wyliodrin-app-server /opt/wyliodrin/wyliodrin-app-server
 
 ln -s /opt/wyliodrin /wyliodrin
 
 mkdir /wyliodrin/projects
 mkdir /wyliodrin/projects/build
 
-cd /wyliodrin/wyl
+cd /wyliodrin/wyliodrin-app-server
 
 mkdir /etc/wyliodrin
 echo -n cisco829 > /etc/wyliodrin/boardtype
 cp setup/settings_cisco829.json /etc/wyliodrin
-#cp scripts/cisco829/S99wyliodrin-app-server /etc/rc3.d/
 
-node startup
+cp scripts/cisco829/wyliodrin-app-server /etc/init.d/wyliodrin-app-server
+
+chmod +x /etc/init.d/wyliodrin-app-server
+
+ln -s ../init.d/wyliodrin-app-server /etc/rc2.d/S99wyliodrin-app-server
+ln -s ../init.d/wyliodrin-app-server /etc/rc3.d/S99wyliodrin-app-server
+ln -s ../init.d/wyliodrin-app-server /etc/rc4.d/S99wyliodrin-app-server
+ln -s ../init.d/wyliodrin-app-server /etc/rc5.d/S99wyliodrin-app-server
+#####end wyliodrin-app-server
+
+
---
+++ wyliodrin-app-server/board.js
@@ -0,0 +1,57 @@
+
+"use strict";
+
+var board = {
+	'raspberrypi':
+	{
+		serial:'/dev/ttyAMA0',
+		firmware:'/Arduino/src/Arduino.ino',
+		firmware_makefile: '',
+		signals:'redis',
+		nettype:'iwconfig',
+		shell:'bash',
+		avahi:'publish'
+	},
+	'beagleboneblack':
+	{
+		serial:'/dev/ttyGS0',
+		firmware:'',
+		firmware_makefile: '',
+		signals:'redis',
+		nettype:'iwconfig',
+		shell:'bash',
+		avahi:'publish'
+	},
+	'udooneo':
+	{
+		serial:'/dev/ttyGS0',
+		firmware:'/Arduino/Arduino.ino',
+		firmware_makefile: '',
+		signals:'redis',
+		nettype:'nm',
+		shell:'bash',
+		avahi:'publish'
+	},
+	'arduinoyun':
+	{
+		serial:null,
+		firmware:'/Arduino/Arduino.ino',
+		firmware_makefile: 'arduinoyun.mk',
+		signals:'udp',
+		nettype:'iwconfig',
+		shell:'sh',
+		avahi:'restart'
+	},
+	'cisco829':
+	{
+		serial:null,
+		firmware:'',
+		firmware_makefile: '',
+		signals:'redis',
+		nettype:'iwconfig',
+		shell:'sh',
+		avahi:'restart'
+	}
+};
+
+module.exports = board;
