00dd3a04be987d89220126c4d287a4c1ef399cf7
Razvan <raz@raz.raz>
2016-07-01 15:46:08 +0300

    added node-red in script

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ cp -a share/. /usr/share/
 chmod +x /usr/bin/node
 
 cd ..
+rm -rf node
 #####end node
 
 #####start redis
@@ -32,6 +33,7 @@ ln -s ../init.d/redis /etc/rc4.d/S90redis
 ln -s ../init.d/redis /etc/rc5.d/S90redis
 
 cd ..
+rm -rf redis
 
 #end redis
 
@@ -56,7 +58,15 @@ ln -s ../init.d/avahi /etc/rc5.d/S98avahi
 
 
 cd ..
+rm -rf avahi
+
 ####end avahi
+
+
+#####start node-red
+cp -rp node-red/wyliodrin /wyliodrin/node-red
+rm -rf node-red
+#####end node-red
 
 
 #####start wyliodrin-app-server
@@ -64,7 +74,7 @@ mkdir /opt/wyliodrin
 mkdir /usr/wyliodrin
 
 cp -rp wyliodrin-app-server /usr/wyliodrin/wyliodrin-app-server
-
+rm -rf wyliodrin-app-server
 ln -s /opt/wyliodrin /wyliodrin
 
 mkdir /wyliodrin/projects
@@ -85,6 +95,7 @@ ln -s ../init.d/wyliodrin-app-server /etc/rc3.d/S97wyliodrin-app-server
 ln -s ../init.d/wyliodrin-app-server /etc/rc4.d/S97wyliodrin-app-server
 ln -s ../init.d/wyliodrin-app-server /etc/rc5.d/S97wyliodrin-app-server
 #####end wyliodrin-app-server
+
 
 /etc/init.d/redis start
 /etc/init.d/wyliodrin-app-server
